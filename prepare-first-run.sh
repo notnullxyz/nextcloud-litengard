6d3f2c8cc6162a66a6fecacd42f1f42c0f2c9ebb
Marlon v/d Linde <marlonv@protonmail.com>
2018-11-22 20:15:00 +0000

    Add sudo on prepare script to resolve permission issue. Add mailer to docker-compose to send mail through nextcloud

--- prepare-first-run.sh
+++ prepare-first-run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-NEXTCLOUD_DATA_DIR=./opt/cloud_data
+NEXTCLOUD_DATA_DIR=/mnt/volume_one/mash
 
 echo
 read -p "Only run this preparation script from the directory you want to launch docker-compose up from. Press a key to continue" -n1 -s
 echo
 read -p "Press enter if you are sure. A directory called $NEXTCLOUD_DATA_DIR will be prepared here, AFTER which you can run docker-compose up -d for the first time" -n1 -s
 echo
-mkdir -p $NEXTCLOUD_DATA_DIR
+sudo mkdir -p $NEXTCLOUD_DATA_DIR
 sudo chown www-data:www-data $NEXTCLOUD_DATA_DIR
 echo
 echo " Done, now run docker-compose up -d"
